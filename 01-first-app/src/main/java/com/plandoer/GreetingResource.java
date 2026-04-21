1ff7bf4dc31667fdbe78e3b2c68a50a9f5a941b4
yeminko <yeminko47@gmail.com>
2026-04-21 07:39:58 +0630

    feat: add greeting configuration and update GreetingResource to use it

--- 01-first-app/src/main/java/com/plandoer/GreetingResource.java
+++ 01-first-app/src/main/java/com/plandoer/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.plandoer;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,12 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello World!";
+        return greeting;
     }
 }
