34839eeaf2335c3a92371d7c16733713b9253d77
yeminko <yeminko47@gmail.com>
2026-04-27 08:11:04 +0630

    feat: add OIDC dependency and implement /whoami endpoint in GreetingResource

--- 01-first-app/src/main/java/com/plandoer/GreetingResource.java
+++ 01-first-app/src/main/java/com/plandoer/GreetingResource.java
@@ -1,11 +1,15 @@
 package com.plandoer;
 
+import java.security.Principal;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/hello")
 public class GreetingResource {
@@ -18,4 +22,18 @@ public class GreetingResource {
     public String hello() {
         return greeting;
     }
+
+    @GET
+    @Path("/whoami")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String whoAmI(@Context SecurityContext securityContext) {
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        
+        if (userPrincipal != null) {
+            return userPrincipal.getName();
+        } else {
+            return "Anonymous";
+        }
+    }
 }
+
