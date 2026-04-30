314fd74a97a1762d5c33103c118740c6c4cf4739
yeminko <yeminko47@gmail.com>
2026-04-30 08:55:46 +0630

    fix: update HTTP port configuration and adjust greeting message in tests

--- 01-first-app/src/test/java/com/plandoer/GreetingResourceTest.java
+++ 01-first-app/src/test/java/com/plandoer/GreetingResourceTest.java
@@ -1,10 +1,10 @@
 package com.plandoer;
 
-import io.quarkus.test.junit.QuarkusTest;
+import static org.hamcrest.CoreMatchers.is;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.junit.QuarkusTest;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class GreetingResourceTest {
@@ -14,7 +14,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Hello configuration"));
     }
 
 }
