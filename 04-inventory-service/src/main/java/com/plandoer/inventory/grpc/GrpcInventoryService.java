5ce85bc3671cec41d8dfc0d599ee84881b2ded37
yeminko <yeminko47@gmail.com>
2026-06-12 16:06:34 +0630

    feat: add Maven Wrapper and initial project setup for 05-inventory-cli
    - Added mvnw and mvnw.cmd scripts for Maven Wrapper support.
    - Created pom.xml for project dependencies and build configuration.
    - Added Dockerfiles for JVM, legacy JAR, native, and micro image builds.
    - Implemented gRPC proto definitions in inventory.proto for car management.
    - Added application.properties for configuration.

--- 04-inventory-service/src/main/java/com/plandoer/inventory/grpc/GrpcInventoryService.java
+++ 04-inventory-service/src/main/java/com/plandoer/inventory/grpc/GrpcInventoryService.java
@@ -1,6 +1,5 @@
 package com.plandoer.inventory.grpc;
 
-
 import java.util.Optional;
 
 import com.plandoer.inventory.database.CarInventory;
@@ -12,6 +11,7 @@ import com.plandoer.inventory.model.RemoveCarRequest;
 
 import io.quarkus.grpc.GrpcService;
 import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 
@@ -20,21 +20,45 @@ public class GrpcInventoryService implements InventoryService {
     @Inject
     CarInventory inventory;
 
+    // Unary version of add method
+    // @Override
+    // public Uni<CarResponse> add(InsertCarRequest request) {
+    //     Car car = new Car();
+    //     car.licensePlateNumber = request.getLicensePlateNumber();
+    //     car.manufacturer = request.getManufacturer();
+    //     car.model = request.getModel();
+    //     car.id = CarInventory.ids.incrementAndGet();
+    //     Log.info("Persisting " + car);
+    //     inventory.getCars().add(car);
+    //     return Uni.createFrom().item(CarResponse.newBuilder()
+    //             .setLicensePlateNumber(car.licensePlateNumber)
+    //             .setManufacturer(car.manufacturer)
+    //             .setModel(car.model)
+    //             .setId(car.id)
+    //             .build());
+    // }
+
+    // Streaming version of add method
     @Override
-    public Uni<CarResponse> add(InsertCarRequest request) {
-        Car car = new Car();
-        car.licensePlateNumber = request.getLicensePlateNumber();
-        car.manufacturer = request.getManufacturer();
-        car.model = request.getModel();
-        car.id = CarInventory.ids.incrementAndGet();
-        Log.info("Persisting " + car);
-        inventory.getCars().add(car);
-        return Uni.createFrom().item(CarResponse.newBuilder()
-                .setLicensePlateNumber(car.licensePlateNumber)
-                .setManufacturer(car.manufacturer)
-                .setModel(car.model)
-                .setId(car.id)
-                .build());
+    public Multi<CarResponse
+    > add(Multi<InsertCarRequest> requests) {
+        return requests
+                .map(request -> {
+                    Car car = new Car();
+                    car.licensePlateNumber = request.getLicensePlateNumber();
+                    car.manufacturer = request.getManufacturer();
+                    car.model = request.getModel();
+                    car.id = CarInventory.ids.incrementAndGet();
+                    return car;
+                }).onItem().invoke(car -> {
+                    Log.info("Persisting " + car);
+                    inventory.getCars().add(car);
+                }).map(car -> CarResponse.newBuilder()
+                        .setLicensePlateNumber(car.licensePlateNumber)
+                        .setManufacturer(car.manufacturer)
+                        .setModel(car.model)
+                        .setId(car.id)
+                        .build());
     }
 
     @Override
